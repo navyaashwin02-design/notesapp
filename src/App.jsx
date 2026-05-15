9ec30e8f45a1a4a5129e2a9d5e91444b2d1ea80d
Navya <navyaashwin02@gmail.com>
2026-05-15 16:39:59 +0530

    changes for amplify

--- src/App.jsx
+++ src/App.jsx
@@ -118,5 +118,19 @@ function App() {
     </>
   )
 }
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+        <h1>Hello from Amplify</h1>
+      </header>
+    </div>
+  );
+}
+export default App;
+
 
 export default App
