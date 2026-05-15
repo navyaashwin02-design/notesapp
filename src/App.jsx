b714acc022ccb6ba1d76981b5681db3d49dda1a4
Navya <navyaashwin02@gmail.com>
2026-05-15 16:54:54 +0530

    changes for amplify

--- src/App.jsx
+++ src/App.jsx
@@ -99,25 +99,7 @@ function App() {
             </li>
             <li>
               <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
-}
+                
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 function App() {
@@ -133,4 +115,3 @@ function App() {
 export default App;
 
 
-export default App
